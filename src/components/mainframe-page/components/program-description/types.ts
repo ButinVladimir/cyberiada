d41bc770410cfbd18599f80c0b3f02eed9f66e8d
Vladimir Butin <15980943+ButinVladimir@users.noreply.github.com>
2024-12-15 20:59:04 +0100

    Remove unused component

--- src/components/mainframe-page/components/program-description/types.ts
+++
@@ -1,5 +0,0 @@
-export enum ProgramDescriptionType {
-  'program-description' = 'program-description',
-  'program-comparison' = 'program-comparison',
-  'process-comparison' = 'process-comparison',
-}
